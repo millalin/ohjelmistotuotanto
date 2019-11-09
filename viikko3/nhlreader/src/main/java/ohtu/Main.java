9d4d994143118e6c536f2cf0e7cb82dc51bfe153
Milla Lintunen <milla.lintunen@hotmail.com>
2019-11-09 16:17:42 +0200

    teht2

--- viikko3/nhlreader/src/main/java/ohtu/Main.java
+++ viikko3/nhlreader/src/main/java/ohtu/Main.java
@@ -4,6 +4,9 @@ import com.google.gson.Gson;
 import org.apache.http.client.fluent.Request;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -15,11 +18,13 @@ public class Main {
         //System.out.println( bodyText );
 
         Gson mapper = new Gson();
-        Player[] players = mapper.fromJson(bodyText, Player[].class);
+        //Player[] players = mapper.fromJson(bodyText, Player[].class);
+        List<Player> players = Arrays.asList(mapper.fromJson(bodyText, Player[].class));
+        Collections.sort(players);
 
-        for (int i = 0; i < players.length; i++) {
-          if ( players[i].getNationality().equals("FIN")) {
-              System.out.println(players[i]);
+        for (int i = 0; i < players.size(); i++) {
+          if ( players.get(i).getNationality().equals("FIN")) {
+              System.out.println(players.get(i));
           }
             
         }
--- viikko3/nhlreader/src/main/java/ohtu/Player.java
+++ viikko3/nhlreader/src/main/java/ohtu/Player.java
@@ -1,7 +1,7 @@
 
 package ohtu;
 
-public class Player {
+public class Player implements Comparable<Player> {
     public String name;
     public String team;
     public int goals;
@@ -9,7 +9,17 @@ public class Player {
     public int penalties;
     public String nationality;
     public String birthday;
-    
+
+    public Player (String name, String team, int goals, int assists,
+                   int penalties, String nationality, String birthday) {
+        this.name = name;
+        this.team = team;
+        this.goals = goals;
+        this. assists = assists;
+        this.penalties = penalties;
+        this.nationality = nationality;
+        this.birthday = birthday;
+    }
 
     public void setName(String name) {
         this.name = name;
@@ -18,14 +28,24 @@ public class Player {
     public String getName() {
         return name;
     }
+
+    public int getTotal() {
+        return this.goals + this.assists;
+    }
     
     public String getNationality() {
         return nationality;
     }
 
     @Override
-    public String toString() {
-        return name + " Team: " + team + " Goals: "+goals + " Assists: " + assists;
+    public int compareTo(Player player) {
+        return Integer.compare(player.getTotal(), this.getTotal());
     }
-      
+
+    @Override
+    public String toString() {
+        return name + "  " +team + "  " + goals + " + " + assists + " = " + this.getTotal();
+    }
+
+
 }
