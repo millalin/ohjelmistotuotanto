49b829b94d578c45b082cdd9d542dea7b23d832b
Milla Lintunen <milla.lintunen@hotmail.com>
2019-12-02 10:42:15 +0200

    teht3

--- viikko6/QueryLanguage/src/main/java/statistics/Main.java
+++ viikko6/QueryLanguage/src/main/java/statistics/Main.java
@@ -12,8 +12,12 @@ public class Main {
         Statistics stats = new Statistics(new PlayerReaderImpl(url));
 
         Matcher m = new And(
-                new HasFewerThan(1, "goals"),
-                new PlaysIn("NYR")
+                new HasAtLeast(20, "points"),
+                new Or(
+                        new PlaysIn("NYR"),
+                        new PlaysIn("NYI"),
+                        new PlaysIn("NJD")
+                )
         );
 
 
